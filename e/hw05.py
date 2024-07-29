5d841509b490c30e633412e028f8130550b42886
msconfiggg <1466876532@qq.com>
2024-07-29 09:59:00 +0800

    second commit

--- e/hw05.py
+++ e/hw05.py
@@ -479,3 +479,23 @@ def replace(s, t, i, j):
         s.rest, end.rest = t, s.rest
 
 #12
+def link_insert(lnklst, value, before):
+    """Return a linked list identical to LNKLST, but with VALUE inserted just
+    before the first occurrence of BEFORE in the list, if any. The returned
+    list is identical to LNKLST if BEFORE does not occur in LNKLST.
+    The operation is non-destructive.
+    >>> L = link(2, link(3, link(7, link(1))))
+    >>> print_link(L)
+    (2, 3, 7, 1)
+    >>> Q = link_insert(L, 19, 7)
+    >>> print_link(Q)
+    (2, 3, 19, 7, 1)
+    >>> print_link(link_insert(L, 19, 20))
+    (2, 3, 7, 1)
+    """
+    if lnklst == Link.empty:
+        return lnklst
+    elif lnklst.first == before:
+        return Link(value, lnklst)
+    else:
+        return Link(lnklst.first, link_insert(lnklst.rest, value, before))
