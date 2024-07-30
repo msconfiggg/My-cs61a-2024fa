fb2076907d8fb4f915b9ca1cff2680979508b1b7
msconfiggg <1466876532@qq.com>
2024-07-30 11:12:26 +0800

    sorted

---
+++ e/discussion/disc04.py
@@ -0,0 +1,139 @@
+#1
+def paths(m, n):
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    if m == 1 and n == 1:
+        return 1
+    if m == 1 and n != 1:
+        return paths(m, n - 1)
+    if m != 1 and n == 1:
+        return paths(m - 1, n)
+    else:
+        return paths(m - 1, n) + paths(m, n - 1)
+
+#2
+def even_weighted_loop(s):
+    """
+    >>> x = [1, 2, 3, 4, 5, 6]
+    >>> even_weighted_loop(x)
+    [0, 6, 20]
+    """
+    "*** YOUR CODE HERE ***"
+    result = []
+    for i in range(len(s)):
+        if i % 2 == 0:
+            result += [i * s[i]]
+    return result
+
+def even_weighted_comprehension(s):
+    """
+    >>> x = [1, 2, 3, 4, 5, 6]
+    >>> even_weighted_comprehension(x)
+    [0, 6, 20]
+    """
+    return [i * s[i] for i in range(len(s)) if i % 2 == 0]
+
+#3
+def has_path(t, p):
+    """Return whether tree t has a path from the root with labels p.
+
+    >>> t2 = tree(5, [tree(6), tree(7)])
+    >>> t1 = tree(3, [tree(4), t2])
+    >>> has_path(t1, [5, 6])        # This path is not from the root of t1
+    False
+    >>> has_path(t2, [5, 6])        # This path is from the root of t2
+    True
+    >>> has_path(t1, [3, 5])        # This path does not go to a leaf, but that's ok
+    True
+    >>> has_path(t1, [3, 5, 6])     # This path goes to a leaf
+    True
+    >>> has_path(t1, [3, 4, 5, 6])  # There is no path with these labels
+    False
+    """
+    if p == [label(t)]:  # when len(p) is 1
+        return True
+    elif label(t) != p[0]:
+        return False
+    else:
+        "*** YOUR CODE HERE ***"
+        for branch in branches(t):
+            if has_path(branch, p[1:]):
+                return True
+        return False
+
+#4
+def find_path(t, x):
+    """
+    >>> t2 = tree(5, [tree(6), tree(7)])
+    >>> t1 = tree(3, [tree(4), t2])
+    >>> find_path(t1, 5)
+    [3, 5]
+    >>> find_path(t1, 4)
+    [3, 4]
+    >>> find_path(t1, 6)
+    [3, 5, 6]
+    >>> find_path(t2, 6)
+    [5, 6]
+    >>> print(find_path(t1, 2))
+    None
+    """
+    if label(t) == x:
+        return [label(t)]
+    for branch in branches(t):
+        path = find_path(branch, x)
+        if path:
+            return [label(t)] + path
+    return None
+
+#5
+def sprout_leaves(t, leaves):
+    """Sprout new leaves containing the labels in leaves at each leaf of
+    the original tree t and return the resulting tree.
+
+    >>> t1 = tree(1, [tree(2), tree(3)])
+    >>> print_tree(t1)
+    1
+      2
+      3
+    >>> new1 = sprout_leaves(t1, [4, 5])
+    >>> print_tree(new1)
+    1
+      2
+        4
+        5
+      3
+        4
+        5
+
+    >>> t2 = tree(1, [tree(2, [tree(3)])])
+    >>> print_tree(t2)
+    1
+      2
+        3
+    >>> new2 = sprout_leaves(t2, [6, 1, 2])
+    >>> print_tree(new2)
+    1
+      2
+        3
+          6
+          1
+          2
+    """
+    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        new_branches = [tree(leaf) for leaf in leaves]
+        return tree(label(t), new_branches)
+    else:
+        new_branches = [sprout_leaves(branch, leaves) for branch in branches(t)]
+        return tree(label(t), new_branches)
